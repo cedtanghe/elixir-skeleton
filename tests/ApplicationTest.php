c186ea40335cd1bb72cbdfcf80d7f546931034b3
cedtanghe <ced.tanghe@gmail.com>
2016-01-05 15:26:31 +0100

    Add default test file

---
+++ tests/ApplicationTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Test;
+
+use PHPUnit_Framework_TestCase;
+
+class ApplicationTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $output = 'ready';
+        $this->assertEquals('ready', $output);
+    }
+}
