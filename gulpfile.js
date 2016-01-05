5eb7f46f57bfb19096561f6259dd9281de5703b2
cedtanghe <ced.tanghe@gmail.com>
2016-01-05 14:31:26 +0100

    reorganization of folders

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jshintStylish = require('jshint-stylish');
+ 
+// Lint
+gulp.task('lint', function() 
+{
+    gulp.src('medias/scripts/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+ 
+// Launch all
+gulp.task('default', ['lint'], function(){});
